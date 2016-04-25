7a2632bbac235191f2e14cae828d7cfa4d23e4b6
Aaron Esteban Mora <ennety@gmail.com>
2016-04-25 15:57:44 +0200

    Getters y setters ya realizados en la clase CarnetCruzRoja

--- src/com/ceste/CarnetCruzRoja.java
+++ src/com/ceste/CarnetCruzRoja.java
@@ -5,4 +5,67 @@ package com.ceste;
  */
 
 public class CarnetCruzRoja {
+    private String dni = "";
+    private String apellidos = "";
+    private String nombre = "";
+    private String provincia = "";
+    private String localidad = "";
+    private String servicio = "";
+    private String fecha = "";
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setServicio(String servicio) {
+        this.servicio = servicio;
+    }
+
+    public String getServicio() {
+        return servicio;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
 }
--- src/com/ceste/Main.java
+++ src/com/ceste/Main.java
@@ -3,6 +3,12 @@ package com.ceste;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    CarnetCruzRoja carnet = new CarnetCruzRoja();
+        CarnetCruzRoja carnet1 = new CarnetCruzRoja();
+        carnet.setApellidos("pepe");
+        carnet1.setApellidos("juan");
+        System.out.println(carnet.getApellidos());
+        System.out.println(carnet1.getApellidos());
     }
+
 }
